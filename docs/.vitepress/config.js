aaad1d39e2c360f45d4dd4647d15a2e5db3a0ed1
凤之兮原 <1577310596@qq.com>
2022-08-30 21:45:47 +0800

    feat: add themeConfig

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,18 @@
 export default {
   title: 'VitePress',
-  description: 'Just playing around.'
+  description: 'Just playing around.',
+  themeConfig: {
+    sidebar: [{
+      text: 'Guide',
+      items: [{
+          text: 'Introduction',
+          link: '/'
+        },
+        {
+          text: 'TDD',
+          link: '/TDD'
+        }
+      ]
+    }]
+  }
 }
