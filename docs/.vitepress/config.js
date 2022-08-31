5c0fe8a31a9f0e22a9d97ee71f9809ba33bdf88b
凤之兮原 <1577310596@qq.com>
2022-08-31 19:42:30 +0800

    feat: add guide

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,18 +1,23 @@
 export default {
-  title: 'VitePress',
+  title: '凤之兮原の言葉',
   description: 'Just playing around.',
   themeConfig: {
     sidebar: [{
       text: 'Home',
       items: [{
-          text: '介绍',
-          link: '/'
-        },
-        {
-          text: 'TDD 开发思想',
-          link: '/TDD'
-        }
+        text: '介绍',
+        link: '/'
+      },
+      {
+        text: 'TDD 开发思想',
+        link: '/TDD'
+      }
       ]
-    }]
+    }],
+    nav: [
+      { text: 'Guide', link: '/guide' },
+      { text: 'Configs', link: '/configs' },
+      { text: 'Changelog', link: 'https://github.com/...' }
+    ]
   }
 }
